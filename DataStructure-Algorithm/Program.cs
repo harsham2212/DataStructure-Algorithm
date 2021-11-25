e5d8b608a684760b01c6ed639f154575708e1f20
Harsh Singh <kmrharsh50@gmail.com>
2021-11-25 07:22:54 +0530

    [Harsh]Added:UnOrdered List DataStructure-Algorithm feature implemented

---
+++ DataStructure-Algorithm/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DataStructure_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                bool flag = true;
+                while (flag)
+                {
+                    Console.WriteLine("Enter Progran number to get excuted \n1. Unordred list \n2. Exit");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            UnorderedList unordered = new UnorderedList();
+                            unordered.Operation();
+                            break;
+                        case 2:
+                            flag = false;
+                            break;
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DataStructure-Algorithm/UnorderedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DataStructure_Algorithm
+{
+    internal class UnorderedList
+    {
+        public void Operation()
+        {
+            string data = File.ReadAllText(@"E:\BridgeLAbz\Git\DataStructure-Algorithm\DataStructure-Algorithm\Utility\message.txt");
+            Console.WriteLine("Reading File from the Message.txt");
+            Console.WriteLine(data);
+            string[] words = data.Split(' ');
+            LinkedList<string> linkedlist = new LinkedList<string>();
+            
+            foreach (string item in words)
+            {
+                linkedlist.AddLast(item);
+            }
+           
+            Display(linkedlist);
+            Console.WriteLine("Enter the string want to search");
+            string input = Console.ReadLine();
+           
+            if (linkedlist.Contains(input))
+            {
+                linkedlist.Remove(input);
+                Console.WriteLine($"{input} Removed from the list");
+            }
+            else
+            {
+                linkedlist.AddLast(input);
+                Console.WriteLine($"{input} Added to the list");
+            }
+
+            Console.WriteLine("Changes after Unordering");
+            Display(linkedlist);
+        }
+
+        private void Display(LinkedList<string> linkedlist)
+        {
+            foreach (var item in linkedlist)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
